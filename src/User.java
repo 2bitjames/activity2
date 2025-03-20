5be04bfa286dd66b12045745143d88803f53e158
Jonas <jonas.bardini@gmail.com>
2025-03-20 13:26:46 +1000

    example commit to jonas branch

--- src/User.java
+++ src/User.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class User {
+public class User { // example update -> dawdawdwadawdawdwad
     private String username;
     private String password;
     private ArrayList<ToDoItem> toDoItems;
